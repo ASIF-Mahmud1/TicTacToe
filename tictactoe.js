d676c9a30d975f6e743ae1508bfb862cd32d4df1
cis101 <cis101@secs.iub.edu.bd>
2018-02-07 09:34:31 +0600

    Practice

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,10 @@
-//JS code goes here
+var turn=false;
+function play(event)
+{
+  if(! event.target.innerText)
+  {	
+  console.log(event);
+  event.target.innerText= turn ? "0": "X";
+  turn= !turn;
+  }
+}
